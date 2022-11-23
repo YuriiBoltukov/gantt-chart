567f6f34d7a3e3ce57ecf639202a4cb856e293ef
YuriiBoltukov <kolomiha898@gmail.com>
2022-11-24 01:28:18 +0300

    production

--- src/environment/environment.ts
+++
@@ -1,1 +0,0 @@
-export const host = 'http://82.202.204.94/tmp/';
--- src/state/actions/actionTypes.ts
+++
@@ -1,3 +0,0 @@
-export const SET_TASKS = 'SET_TASKS';
-export const SET_ACTIVE_STATUS = 'SET_ACTIVE_STATUS';
-export const SET_MOCK_DATA = 'SET_MOCK_DATA';
