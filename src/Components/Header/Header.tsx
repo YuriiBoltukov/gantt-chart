283c60c1a8aa656e1aba191c1e01026f9d89a7fb
YuriiBoltukov <kolomiha898@gmail.com>
2022-11-23 13:51:53 +0300

    feat: download schedule

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,10 +3,13 @@ import { exportIcon } from '../../assets/Images/Icons/icons';
 import { IDataResponse } from '../../interface/interface';
 
 export const Header = ({ project, period }: IDataResponse) => {
+	function exportView() {
+		window.print();
+	}
 	return (
 		<div className={style.header}>
 			<h1 className={style.title}>{`${project} / ${period}`}</h1>
-			<button className={style.button}>
+			<button className={style.button} onClick={exportView}>
 				{exportIcon}
 				<span>Export</span>
 			</button>
